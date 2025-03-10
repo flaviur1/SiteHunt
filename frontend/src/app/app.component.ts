ad149ae3db24f99fb040b975fac0b3fc2f2285cc
Romitan Flaviu <flaviu.romitan23@gmail.com>
2025-03-11 01:03:57 +0200

    added primeNG and primeicons to the dependencies, created the search-bar component, added a background image and some simple css for everything

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
+  // onClick() {
+  //   console.log('clicked');
+  // }
 }
--- frontend/src/app/components/search-bar/search-bar.component.ts
+++ frontend/src/app/components/search-bar/search-bar.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
+  standalone: true,
   imports: [],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
-export class SearchBarComponent {
-
-}
+export class SearchBarComponent {}
